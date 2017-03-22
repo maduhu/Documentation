92c9cb97c2f6264c34f80e9868366736bdee8234
Bill Hodghead <bhcrosslake@users.noreply.github.com>
2017-03-21 19:01:34 -0700

    Add more pages and other repos
    Using regex expressions for image substitution
    Added more pages from Docs repo
    Added pages for other repos

--- CopyImages.sh
+++ CopyImages.sh
@@ -5,7 +5,7 @@ cp Docs/DFSP/PendingTransactions/*.png ./images
 cp Docs/DFSP/BulkPayment/src/*.png ./images
 cp Docs/DFSP/USSD/*.PNG ./images
 cp Docs/DFSP/USSD/*.JPG ./images
-cp "Docs/DFSP/Pending Transactions Api/diagrams"/*.png   ./images
+cp "Docs/DFSP/Pending Transactions Api/diagrams"/*.png ./images
 cp Docs/DFSP/ManageAcccoutHolders/*.jpg ./images
 cp "Docs/DFSP/GSMA MM & L1P DFSP API"/*.jpg ./images
 cp Docs/CentralDirectory/*.png ./images
@@ -13,3 +13,4 @@ cp Docs/CentralLedger/*.png ./images
 cp Docs/CentralRules/*.png ./images
 cp Docs/AWS/Infrastructure/*.png ./images
 cp Docs/mule/*.png ./images
+cp Docs/JMeter/media/*.jpg ./images
