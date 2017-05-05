e1c2e7841d969519bf040e38aba020773be17f37
mDuo13 <mduo13@gmail.com>
2017-05-04 17:22:31 -0700

    exportdocs: cleanup wikilinks filter

--- ExportDocs/pdf_templates/filters/filter_wikilinks.py
+++ ExportDocs/pdf_templates/filters/filter_wikilinks.py
@@ -1,7 +1,11 @@
 ################################################################################
 ## Wiki Links Filter
 ##
-## Converts links in gollum (GitHub wiki) format to markdown links
+## Converts links in gollum (GitHub wiki) format to markdown links.
+## Assumes that the page the link should point to has an HTML filename equal to
+## its canonical name with .html as the extension.
+## For example, [[Accessing the Test Environment]] should link to
+## Accessing-the-Test-Environment.html
 ################################################################################
 
 import re
@@ -12,7 +16,7 @@ GOLLUM_LINK = re.compile(r'\[\[([^\]]+)\]\]')
 def filter_markdown(md, **kwargs):
     m = re.search(GOLLUM_LINK, md)
     while m:
-        print("found gollum-style link %s" % m)
+        # print("found gollum-style link %s" % m)
         link_label = m.group(1)
         # Assume the resulting page has a matching filename that ends in .html
         link_path = link_label.replace("/", "-").replace(" ", "-") + ".html"
